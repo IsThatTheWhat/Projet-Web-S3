69baf23e4bbb54cf335eb112805e2e512d1ae657
IsThatTheWhat <livetyan@gmail.com>
2017-06-01 01:00:08 +0200

    sql file

--- projweb.sql
+++ projweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 31 Mai 2017 à 09:20
+-- Généré le :  Mer 31 Mai 2017 à 22:59
 -- Version du serveur :  5.7.14
 -- Version de PHP :  7.0.10
 
@@ -40,7 +40,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$0oONvsQvG6eFqmBWRczkG.A8z8Iz6bydiem245S.K5nk0.rzP/7x2', 'isOWuqedn2QqhnX9DvkFNPbq4PHRWZjdQyKAmuxuqrBsZdCezb6lUiPke1pS', '2017-05-17 14:56:32', '2017-05-17 14:56:32');
+(1, 'admin', '$2y$10$ZDgzjcJa0uzjvvDgPWxtOOoUIoUn/zfBMCarfZU7Z3mD.BwKxHNdq', 'jlBXKCEeDqPsknlMdWjyaYcu6fN2S5UM8prxYYAh7QBjsDG6xac8EK7ajEa2', '2017-05-31 20:55:30', '2017-05-31 20:55:30');
 
 -- --------------------------------------------------------
 
@@ -54,14 +54,6 @@ CREATE TABLE `allows` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Contenu de la table `allows`
---
-
-INSERT INTO `allows` (`product_id`, `user_id`, `created_at`, `updated_at`) VALUES
-(2, 1, NULL, NULL),
-(3, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -77,13 +69,6 @@ CREATE TABLE `comments` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Contenu de la table `comments`
---
-
-INSERT INTO `comments` (`id`, `content`, `user_id`, `product_id`, `created_at`, `updated_at`) VALUES
-(1, '<p>Salut, super <strong>produit!!!</strong></p>', 1, 1, '2017-05-18 05:12:54', '2017-05-18 05:12:54');
 
 -- --------------------------------------------------------
 
@@ -102,16 +87,16 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(123, '2014_10_12_000000_create_users_table', 1),
-(124, '2014_10_12_100000_create_password_resets_table', 1),
-(125, '2017_04_02_141050_create_admins_table', 1),
-(126, '2017_04_02_162522_create_type_products_table', 1),
-(127, '2017_04_02_162636_create_products_table', 1),
-(128, '2017_04_03_124108_create_orders_table', 1),
-(129, '2017_04_03_125420_create_states_table', 1),
-(130, '2017_04_09_191647_create_table_allows', 1),
-(131, '2017_04_09_194333_create_table_comments', 1),
-(132, '2017_04_12_180401_add_columns_to_user_table', 1);
+(188, '2014_10_12_000000_create_users_table', 1),
+(189, '2014_10_12_100000_create_password_resets_table', 1),
+(190, '2017_04_02_141050_create_admins_table', 1),
+(191, '2017_04_02_162522_create_type_products_table', 1),
+(192, '2017_04_02_162636_create_products_table', 1),
+(193, '2017_04_03_124108_create_orders_table', 1),
+(194, '2017_04_03_125420_create_states_table', 1),
+(195, '2017_04_09_191647_create_table_allows', 1),
+(196, '2017_04_09_194333_create_table_comments', 1),
+(197, '2017_04_12_180401_add_columns_to_user_table', 1);
 
 -- --------------------------------------------------------
 
@@ -121,22 +106,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `orders` (
   `id` int(10) UNSIGNED NOT NULL,
-  `price` double(8,2) DEFAULT NULL,
+  `price` double DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   `state_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Contenu de la table `orders`
---
-
-INSERT INTO `orders` (`id`, `price`, `user_id`, `state_id`, `created_at`, `updated_at`) VALUES
-(1, 50000.00, 1, 1, '2017-05-18 04:54:42', '2017-05-18 04:54:42'),
-(2, 50000.00, 1, 1, '2017-05-18 04:55:05', '2017-05-18 04:55:05'),
-(3, 50000.00, 1, 1, '2017-05-18 04:55:12', '2017-05-18 04:55:12'),
-(4, 370000.00, 1, 2, '2017-05-18 04:55:53', '2017-05-18 05:11:59');
 
 -- --------------------------------------------------------
 
@@ -151,17 +126,6 @@ CREATE TABLE `order_product` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Contenu de la table `order_product`
---
-
-INSERT INTO `order_product` (`order_id`, `product_id`, `quantity`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, NULL, NULL),
-(2, 1, 1, NULL, NULL),
-(3, 1, 1, NULL, NULL),
-(4, 2, 4, NULL, NULL),
-(4, 3, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -198,15 +162,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `photo`, `available`, `stock`, `type_id`, `created_at`, `updated_at`) VALUES
-(1, 'Volkswagen - Scirocco', 50000, '1495040694.jpg', 1, 7, 3, '2017-05-17 15:04:54', '2017-05-18 04:55:12'),
-(2, 'Tesla - Model S', 75000, '1495040866.jpg', 0, 0, 2, '2017-05-17 15:07:46', '2017-05-18 04:55:53'),
-(3, 'Tesla - Model 3', 70000, '1495041006.jpg', 1, 5, 2, '2017-05-17 15:10:07', '2017-05-18 04:55:53'),
-(4, 'Peugeot - 5008', 30000, '1495041120.jpg', 1, 15, 4, '2017-05-17 15:12:00', '2017-05-17 15:12:00'),
-(5, 'Alfa Romeo', 55000, '1495041225.jpg', 1, 7, 4, '2017-05-17 15:13:46', '2017-05-17 15:13:46'),
-(6, 'Lamborghini - Murcielago', 1300000, '1495041341.jpg', 1, 2, 1, '2017-05-17 15:15:23', '2017-05-18 04:48:03'),
-(7, 'Mclaren - P1', 1200000, '1495041450.jpg', 1, 2, 1, '2017-05-17 15:17:30', '2017-05-17 15:17:30'),
-(8, 'Audi - S5', 80000, '1495041650.jpg', 1, 9, 3, '2017-05-17 15:20:50', '2017-05-17 15:20:50'),
-(9, 'Renault - Clio 3', 1000, '1495091870.jpg', 1, 150, 3, '2017-05-18 05:17:50', '2017-05-18 05:17:50');
+(1, 'Volkswagen - Scirocco', 50000, 'scirocco.jpg', 1, 10, 1, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(2, 'Audi - S5', 80000, 'audi-s5.jpg', 1, 10, 1, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(3, 'Mclaren - P1', 1200000, 'mclaren-p1.jpg', 1, 3, 2, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(4, 'Lamborghini - Murcielago', 1300000, 'lambo-murcielago.jpg', 1, 3, 2, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(5, 'Tesla - Model S', 75000, 'tesla-s.jpg', 1, 5, 3, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(6, 'Tesla - Model 3', 70000, 'tesla-3.jpg', 1, 5, 3, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(7, 'Peugeot - 5008', 30000, 'peugeot-5008.jpg', 1, 5, 4, '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(8, 'Jeep', 55000, 'jeep.jpg', 1, 5, 4, '2017-05-31 20:55:30', '2017-05-31 20:55:30');
 
 -- --------------------------------------------------------
 
@@ -226,8 +189,8 @@ CREATE TABLE `states` (
 --
 
 INSERT INTO `states` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'A preparer', NULL, NULL),
-(2, 'Expedier', NULL, NULL);
+(1, 'A preparer', '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(2, 'Expedier', '2017-05-31 20:55:30', '2017-05-31 20:55:30');
 
 -- --------------------------------------------------------
 
@@ -247,10 +210,10 @@ CREATE TABLE `type_products` (
 --
 
 INSERT INTO `type_products` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Sport', '2017-05-17 14:58:21', '2017-05-17 14:58:21'),
-(2, 'Electric', '2017-05-17 14:59:22', '2017-05-17 14:59:22'),
-(3, 'Coupe', '2017-05-17 15:01:10', '2017-05-17 15:01:10'),
-(4, 'SUV', '2017-05-17 15:02:33', '2017-05-17 15:02:33');
+(1, 'Coupe', '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(2, 'Sport', '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(3, 'Electric', '2017-05-31 20:55:30', '2017-05-31 20:55:30'),
+(4, 'Suv', '2017-05-31 20:55:30', '2017-05-31 20:55:30');
 
 -- --------------------------------------------------------
 
@@ -277,8 +240,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `lastName`, `address`, `about`, `picture`) VALUES
-(1, 'Yan', 'yan@yan.com', '$2y$10$0bBUsPBTWsJcENIdmd.sUux9UXHghZOQOZHHFHecIe9mwnaT7Rn/K', 'Yzoadz17tVdeKEfJabXmAckyEnXqQHygR9dpBXaviEImw46ZJztpJZwzHT1C', '2017-05-18 04:38:28', '2017-05-27 16:59:31', 'Livet', '12 rue Belfort', '<p>I love <strong>Chocolate!</strong></p>', '1495089611.jpg'),
-(5, 'yoyo', 'yo@yo.fr', '$2y$10$ODl2EiSEfyZ0IebLxe/7oewJAKA1VDTudb7c8WM92rZql09MY9bqq', NULL, '2017-05-26 05:31:37', '2017-05-26 05:31:37', NULL, NULL, NULL, 'default_profile_pic.png');
+(1, 'yan', 'yan@yan.com', '$2y$10$FXTRA4Q7l4hGDt3o7y5PIuEwSEOpo8..kGTU.MDk1Y8iB5h7TBZZK', 'O0ussG4daTJpQT9d0NN8IIuQfa2siS9QWmvSEpGtTDHwnAL6PTJyywx2y3Jq', '2017-05-31 20:55:30', '2017-05-31 20:55:30', NULL, NULL, NULL, 'default_profile_pic.png');
 
 --
 -- Index pour les tables exportées
@@ -372,22 +334,22 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=198;
 --
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `states`
 --
@@ -402,7 +364,7 @@ ALTER TABLE `type_products`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
